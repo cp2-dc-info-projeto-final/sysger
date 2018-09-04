a95cad7807e5d2b448eeb924fb1ff9dcfbe58f46
Esther Paulino <estherr1202@gmail.com>
2018-09-04 11:15:31 -0300

    Alterações no banco de dados

--- Bancodedados.sql
+++ Bancodedados.sql
@@ -1,17 +1,3 @@
-CREATE TABLE Pessoa_Física (
-
-		id_PF INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
-		cnpj INT(14) NOT NULL,
-		PRIMARY KEY(Id_PF)
-		);
-
-CREATE TABLE Pessoa_Jurídica (
-
-		id_PJ INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
-		cpf INT(11) NOT NULL,
-		PRIMARY KEY(Id_PJ)
-		);
-
 CREATE TABLE Cliente (
 
 		idCliente INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
@@ -21,11 +7,29 @@ CREATE TABLE Cliente (
 		telefone INT(30),
 		dataNasc DATE,
 		email VARCHAR(100),
-		PRIMARY KEY(IdCliente),
-		FOREIGN KEY (Id_PF) REFERENCES Pessoa Física(Id_PF),
-		FOREIGN KEY (Id_PJ) REFERENCES Pessoa Jurídica(Id_PJ)
+		PRIMARY KEY(IdCliente)
 
 		);
+
+CREATE TABLE Pessoa_Fisica (
+
+		id_PF INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
+		cnpj INT(14) NOT NULL,
+		PRIMARY KEY(Id_PF),
+    FOREIGN KEY (Id_Cliente) REFERENCES Cliente(Id_Cliente)
+
+		);
+
+CREATE TABLE Pessoa_Juridica (
+
+		id_PJ INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
+		cpf INT(11) NOT NULL,
+		PRIMARY KEY(Id_PJ),
+    FOREIGN KEY (Id_Cliente) REFERENCES Cliente(Id_Cliente)
+
+		);
+
+
 CREATE TABLE Pagamento (
 
 				idPagamento INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
@@ -53,13 +57,13 @@ CREATE TABLE Gerenciamento (
 
 
 
-CREATE TABLE Serviço (
+CREATE TABLE Servico (
 
-		idServiço INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
+		idServico INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
 		valor DOUBLE NOT NULL,
 		diaVenc INT(30) NOT NULL,
 		dataContrato DATE,
-		PRIMARY KEY(IdServiço),
+		PRIMARY KEY(IdServico),
 		FOREIGN KEY (Id_Pagamento) REFERENCES Pagamento(Id_Pagamento),
 		FOREIGN KEY (Id_Cliente) REFERENCES Cliente(Id_Cliente),
 		FOREIGN KEY (Id_Gerenciamento) REFERENCES Gerenciamento(Id_Gerenciamento)
@@ -72,20 +76,20 @@ CREATE TABLE Celular (
 		numero VARCHAR(30) NOT NULL,
 		email VARCHAR(100),
 		PRIMARY KEY(IdCelular),
-		FOREIGN KEY (Id_Serviço) REFERENCES Serviço(Id_Serviço)
+		FOREIGN KEY (Id_Servico) REFERENCES Serviço(Id_Servico)
 
 		);
 
-CREATE TABLE Veículo (
+CREATE TABLE Veiculo (
 
-		idVeículo INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
+		idVeiculo INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
 		modelo VARCHAR(30),
 		placa VARCHAR(30),
 		cor VARCHAR(30),
 		ano DATE,
 		numRastreador INT(100),
-		PRIMARY KEY(IdVeículo),
-		FOREIGN KEY (Id_Serviço) REFERENCES Serviço(Id_Serviço)
+		PRIMARY KEY(IdVeiculo),
+		FOREIGN KEY (Id_Servico) REFERENCES Serviço(Id_Servico)
 
 
 		);
