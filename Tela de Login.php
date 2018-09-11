7fe84ceb9c02f30ff86f7fe618ed406619a2b2e8
Esther de Lima  Paulino <estherr1202@gmail.com>
2018-09-11 11:46:34 -0300

    Ligação com Banco de Dados

--- Tela de Login.php
+++ Tela de Login.php
@@ -1,50 +1,30 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset= "utf-8"/>
-    <title> SysGER </title>
-		<style>
+<?php
 
-      h1 {Color: black; padding-left: 50px;}
-      body { background-color: #0A0A2A; }
-      div { background-color: #F8E0F7;
-				margin-left: 600px;
-				margin-top: 200px;
-				margin-right: 650px;
-				margin-bottom: 10px;
-				padding: 20px;
-				border { background-color: black;}}
-      form{padding: 50px; padding-top: 10px;}
-       {
-      background-color: #4CAF50;
-        border: none;
-        color: green;
-        padding: 15px 32px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        }
-      .verde{
-				background-color: #A9D0F5;
-				padding-left: 65px;
-				padding-right: 68px;
-				padding-bottom: 5px;
-      }
-		</style>
-	</head>
-	<body>
-    <div>
-		    <h1> Tela de Login</h1>
+function FazerLigação()
+{
+	$bd = new PDO('mysql:host=localhost;dbname=bancosysger;charset=utf8',
+	'bancosysger',
+	'tccsysger');
 
-		      <form action= "" method= "POST">
-		          <input name="CPF/CNPJ" required type="text" value="CPF/CNPJ" minlenght="11" maxlength="14"/></label><br/><br/>
-			        <input name="Senha" required type="text" value="Senha"/minlenght="6" maxlength="12"></label><br/></br>
-              <input class="button verde" type= "button" value= "Entrar"/>
-		      </form>
+	$bd->setAttribute(PDO::ATTR_ERRMODE,
+					  PDO::ERRMODE_EXCEPTION);
 
-            <label>Esqueceu sua senha?</label>
-    </div>
+	return $bd;
+}
 
-	</body>
-</html>
+function PermitirLogin()
+{
+	$bd = FazerLigação();
+
+	$resultados = $bd->query('SELECT * FROM Cliente');
+
+	return $resultados;
+
+	if($resultados = $_REQUEST)
+	{
+	}
+	if{
+
+	}
+}
+?>
