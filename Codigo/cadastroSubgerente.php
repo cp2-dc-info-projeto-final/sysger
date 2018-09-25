3b49856c8bdce5dbc4837a75d9b93959ccca5adb
Esther Paulino <estherr1202@gmail.com>
2018-09-25 20:33:53 -0300

    configurando validações

--- Codigo/cadastroSubgerente.php
+++ Codigo/cadastroSubgerente.php
@@ -30,18 +30,15 @@
 		$erros[] = "Email vazio";
 	}
 	else if (empty(BuscaUsuarioPorEmail($request['email'])) == false) {
-		$erros[] = "Já existe um cliente cadastrado com esse email";
+		$erros[] = "Já existe um subgerente cadastrado com esse email";
 	}
 
-  $cpf_cnpj = $request['cpf_cnpj'];
-	if($cpf_cnpj == false){
+  $cpf = $request['cpf'];
+	if($cpf == false){
 		$erros[] = "Cpf vazio";
 	}
-	else if (empty(BuscaUsuarioPorCPF($cpf_cnpj)) == false) {
+	else if (empty(BuscaUsuarioPorCPF($cpf)) == false) {
 		$erros[] = "Já existe um cliente cadastrado com esse cpf";
-	}
-	else if (empty(BuscaUsuarioPorCNPJ($cpf_cnpj)) == false) {
-		$erros[] = "Já existe um cliente cadastrado com esse cnpj";
 	}
 
 	$senha = $request['senha'];
@@ -68,8 +65,8 @@
 		$data = DateTime::createFromFormat('Y-m-d', $datanasc);
 		$hoje = new DateTime();
 		$diferença = $data -> diff ($hoje);
-		if($anoscorridos < 16){
-			$erros[] = "É necessário ter mais de 16 anos";
+		if($anoscorridos < 18){
+			$erros[] = "É necessário ter mais de 18 anos";
 		}
 	}
 	$telefone = $request['telefone'];
@@ -87,7 +84,7 @@
 	}
 	if (empty($erros))
 	{
-	InsereCliente($request);
+	InsereSubgerente($request);
 	}
 
 ?>
