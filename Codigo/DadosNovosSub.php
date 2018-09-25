33efe5d751e255cb577b98eb9e7030cf59fc09c2
Esther paulino <estherr1202@gmail.com>
2018-09-25 11:38:53 -0300

    Criaçao de cadastro de subgerente

---
+++ Codigo/DadosNovosSub.php
@@ -0,0 +1,61 @@
+!DOCTYPE html>
+<html>
+	<head>
+		<meta charset= "utf-8"/>
+    <title> SysGER </title>
+		<style>
+
+      h1 {Color: black; padding-left: 50px;}
+      body { background-color: #0A0A2A; }
+      div { background-color: #F8E0F7;
+				margin-left: 500px;
+				margin-top: 150px;
+				margin-right: 500px;
+				margin-bottom: 10px;
+				padding: 20px;
+				border { background-color: black;}}
+      form{padding: 50px; padding-top: 10px;}
+       {
+      background-color: #4CAF50;
+        border: none;
+        color: green;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        }
+      .azul{
+				background-color: #A9D0F5;
+				padding-left: 65px;
+				padding-right: 68px;
+				padding-bottom: 5px;
+      }
+		</style>
+	</head>
+	<body>
+    <div>
+		    <h1> Cadastrar Subgerente </h1>
+
+				<!--<?php if($erro != null) { ?>
+					<div>
+						<p> ERRO: <?= $erro ?> </p>
+					</div>
+				<?php } ?>-->
+
+        <form action="cadastroSubgerente.php" method="POST">
+
+              <label>Nome:<input name="nome" type="text" minlength="3" maxlength="35" required/><br/><br/>
+              <label>E-mail:<input name="email" type="email" required/><br/><br/>
+            	<label>Senha: <input name="senha" type="password" minlength="6" maxlength="12" required /><br/><br/>
+              <label>CPF:<input name="cpf" type="text" minlength="11" maxlength="11" required/><br/><br/>
+						  <label>Telefone:<input name="telefone" type="telefone" required/><br/><br/>
+						  <label>Endereço:<input name="endereco" type="endereco" required/><br/><br/>
+            	<label>Data de nascimento: <input name="dataNasc" type="date" required/></label><br/><br/>
+
+            <input type="submit" value="Cadastrar"/>
+</form>
+    </div>
+
+	</body>
+</html>
---
+++ Codigo/cadastroSubgerente.php
@@ -0,0 +1,93 @@
+<?php
+	include 'funcoes.php';
+	$erros = [];
+
+	$request = array_map('trim', $_REQUEST);
+	$request = filter_var_array(
+		$request,
+		[
+			'nome' => FILTER_DEFAULT,
+			'email' => FILTER_VALIDATE_EMAIL,
+      'cpf' => FILTER_DEFAULT,
+			'senha' => FILTER_DEFAULT,
+			'dataNasc' =>  FILTER_DEFAULT,
+			'telefone' =>  FILTER_DEFAULT,
+			'endereco' =>  FILTER_DEFAULT,
+
+		]
+	);
+
+	$nome = $request['nome'];
+	if($nome == false){
+		$erros[] = "Nome vazio";
+	}
+	else if (strlen($nome) < 3 || strlen($nome) > 35 ){
+		$erros[] = "Nome deve ter no mínimo 3 e no máximo 35 caracteres";
+	}
+
+	$email = $request['email'];
+	if($email == false){
+		$erros[] = "Email vazio";
+	}
+	else if (empty(BuscaUsuarioPorEmail($request['email'])) == false) {
+		$erros[] = "Já existe um cliente cadastrado com esse email";
+	}
+
+  $cpf_cnpj = $request['cpf_cnpj'];
+	if($cpf_cnpj == false){
+		$erros[] = "Cpf vazio";
+	}
+	else if (empty(BuscaUsuarioPorCPF($cpf_cnpj)) == false) {
+		$erros[] = "Já existe um cliente cadastrado com esse cpf";
+	}
+	else if (empty(BuscaUsuarioPorCNPJ($cpf_cnpj)) == false) {
+		$erros[] = "Já existe um cliente cadastrado com esse cnpj";
+	}
+
+	$senha = $request['senha'];
+	if($senha == false){
+		$erros[] = "Senha vazia";
+	}
+	else if (strlen ($senha) < 6 || strlen($senha) > 12 ){
+		$erros[] = "Senha deve ter no mínimo 6 e no máximo 12 caracteres";
+	}
+
+	else {
+		$request['senha'] = password_hash($senha, PASSWORD_DEFAULT);
+	}
+
+	$datanasc = $request['dataNasc'];
+
+	$data = DateTime::createFromFormat('Y-m-d', $datanasc);
+
+	if ($data == false){
+		$erros[] = "Valor de Data de Nascimento inválido";
+	}
+	else if(array_key_exists('dia', $_REQUEST)){
+		$dia = $_REQUEST['dia'];
+		$data = DateTime::createFromFormat('Y-m-d', $datanasc);
+		$hoje = new DateTime();
+		$diferença = $data -> diff ($hoje);
+		if($anoscorridos < 16){
+			$erros[] = "É necessário ter mais de 16 anos";
+		}
+	}
+	$telefone = $request['telefone'];
+	if($telefone == false){
+		$erros[] = "Telefone vazio";
+	}
+	$endereco = $request['endereco'];
+	if($endereco == false){
+		$erros[] = "Endereco vazio";
+	}
+
+	foreach($erros as $msg)
+	{
+		echo "<p>$msg</p>";
+	}
+	if (empty($erros))
+	{
+	InsereCliente($request);
+	}
+
+?>
