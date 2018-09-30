cfcce1e5aa1b08e5066555929481fa328e0075d1
Esther Paulino <estherr1202@gmail.com>
2018-09-30 17:28:27 -0300

    2

--- Codigo/DadosServicos.php
+++ Codigo/DadosServicos.php
@@ -11,9 +11,9 @@
       body { background-color: #0A0A2A; }
       div { background-color: #F8E0F7;
 				margin-left: 500px;
-				margin-top: 150px;
-				margin-right: 500px;
-				margin-bottom: 10px;
+				margin-top: 100px;
+				margin-right:500px;
+				margin-bottom: 15px;
 				padding: 20px;
 				border { background-color: black;}}
       form{padding: 50px; padding-top: 10px;}
@@ -49,8 +49,8 @@
 								<option value="4">4</option>
 							</select><br><br>
 
-							<label>Id do cliente:<input name="idCliente" type="text" minlength="1" maxlength="100" required/><br/><br/>
-							<label>Id do Gerenciador:<input name="idGerenciador" type="text" minlength="1" maxlength="100" required/><br/><br/>
+							<label>ID do cliente:<input name="idCliente" type="text" minlength="1" maxlength="100" required/><br/><br/>
+							<label>ID do Gerenciador:<input name="idGerenciador" type="text" minlength="1" maxlength="100" required/><br/><br/>
 							<label>Data do contrato:<input name="dataContrato" type="date" required/><br/><br/>
               <label>Data de vencimento:<input name="diaVenc" type="date" required/><br/><br/>
 						  <label>Valor do serviço:<input name="text" type="double" required/><br/><br/>
