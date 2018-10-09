6e31ed331d189c9062a4a78d2e522dc8a334576c
Brendon Pereira <pereirabrendon26@gmail.com>
2018-10-09 09:51:32 -0300

    Validação Cadastro de Serviços

--- Codigo/Controladores/cadastroServico.php
+++ Codigo/Controladores/cadastroServico.php
@@ -1,4 +1,3 @@
-
 <?php
 	include '../funcoes.php';
 	$erros = [];
@@ -29,6 +28,38 @@
 		$erros[] = "Já existe um cliente cadastrado com esse cnpj";
 	}
 
+	$CpfGerenciador = $request['CpfGerenciador'];
+	$erros[] = BuscaGerente($CpfGerenciador);
+	if($CpfGerenciador == false){
+		$erros[] = "Cpf do gerente vazio";
+	}
+
+	$dataContrato = $request['dataContrato'];
+	if($dataContrato == false){
+		$erros[] = "Data do contrato está vazio";
+	}
+
+	$diaVencimento = $request['diaVenc'];
+	if($diaVencimento == false){
+		$erros[] = "Dia do Vencimento está vazio";
+	}
+
+	$valor = $request['Valor'];
+	if($valor == false){
+		$erros[] = "Valor do serviço está vazio";
+	}
+
+	session_start();
+	if (empty($erros))
+	{
+		InsereServicos($request);
+		$_SESSION['sucesso'] = "Serviço cadastrado com sucesso";
+	}
+	else {
+		$_SESSION['erros'] = $erros;
+	}
+
+	header('Location: ../DadosNovoCliente.php');
 
 	/*
 
