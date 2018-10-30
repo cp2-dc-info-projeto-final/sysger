1c7b245a3b7dba175d84ede951e2b328bcef079a
Brendon Pereira <pereirabrendon26@gmail.com>
2018-10-30 12:22:34 -0300

    Alterações no administrador.

--- Codigo/administrador.php
+++ Codigo/administrador.php
@@ -10,7 +10,6 @@
 		<?php require('BarraNav.php'); ?>
     <div>
 		    <h1> Gerenciamento </h1>
-<<<<<<< HEAD
 
 				<a href="Lista_func.php"> Listagem dos Funcionários</a><br><br>
 			 	<a href ="DadosNovoFunc.php">Cadastrar novos funcionários</a><br><br>
@@ -18,15 +17,6 @@
 				<a href="status_cliente.php">Status dos clientes</a><br><br>
 				<a href ="DadosNovoCliente.php">Cadastrar novos cliente</a><br><br>
 				<a href ="controladores/Sair.php">SAIR</a>
-=======
-				
-				<a href="Lista_func.php"> <button>Listagem dos Funcionários</button></a><br><br>
-			 	<a href ="DadosNovoFunc.php"><button>Cadastrar novos funcionários</button></a><br><br>
-				<a href="Lista_cliente.php"> <button>Listagem dos Clientes</button></a><br><br>
-				<a href="status_cliente.php"> <button>Status dos clientes</button></a><br><br>
-				<a href ="DadosNovoCliente.php"><button>Cadastrar novos cliente</button></a><br><br>
-				<a href ="controladores/Sair.php"><button>SAIR</button></a>
->>>>>>> 607ee416142d526049514036fa9ea9ab1c1d2b90
 
     </div>
 	</body>
