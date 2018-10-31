33cf2f348047d58e90d0c179a5b4be9675f3910a
Jeniffer Gomes <pereirabrendon26@gmail.com>
2018-10-31 07:51:32 -0300

    retirada da margem roxa de histPagamentos.php

--- Codigo/administrador.php
+++ Codigo/administrador.php
@@ -1,4 +1,9 @@
 
+<?php
+require_once ('funcoes.php');
+session_start();
+$id = $_SESSION['id'];
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,12 +14,17 @@
 	<body>
     <div>
 		    <h1> Gerenciamento </h1>
-
+				<?php if(usuarioEhSubgerente($id) == false ) { ?>
 				<a href="Lista_func.php"> <button>Listagem dos Funcionários</button></a><br><br>
-			 	<a href ="DadosNovoFunc.php"><button>Cadastrar novos funcionários</button></a><br><br>
+			<?php } ?>
+			<?php if(usuarioEhSubgerente($id) == false ) { ?>
+			<a href ="DadosNovoFunc.php"><button>Cadastrar novos funcionários</button></a><br><br>
+				<?php } ?>
 				<a href="Lista_cliente.php"> <button>Listagem dos Clientes</button></a><br><br>
 				<a href="status_cliente.php"> <button>Status dos clientes</button></a><br><br>
+					<?php if(usuarioEhSubgerente($id) == false ) { ?>
 				<a href ="DadosNovoCliente.php"><button>Cadastrar novos cliente</button></a><br><br>
+					<?php } ?>
 				<a href ="controladores/Sair.php"><button>SAIR</button></a>
 
     </div>
